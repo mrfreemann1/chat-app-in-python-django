76c86546a157ef50455d62b10a77bb314b7449c8
visovanionut01-bit <visovanionut01@gmail.com>
2026-06-04 03:15:11 +0300

    Update urls.py

--- chat/urls.py
+++ chat/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("chat/<str:username>", views.chat, name="chat"),
     path('api/messages/<int:sender>/<int:receiver>', views.message_list, name='message-detail'),
     path('api/messages', views.message_list, name='message-list'),
+    path('api/utilizatori/', views.api_lista_utilizatori, name='api_utilizatori'),
 ]
