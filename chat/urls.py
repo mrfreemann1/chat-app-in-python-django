b306d337b701182069a0b515c9598af3b88d9260
visovanionut01-bit <visovanionut01@gmail.com>
2026-06-04 05:08:26 +0300

    Update urls.py

--- chat/urls.py
+++ chat/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('api/messages/<int:sender>/<int:receiver>', views.message_list, name='message-detail'),
     path('api/messages', views.message_list, name='message-list'),
     path('api/utilizatori/', views.api_lista_utilizatori, name='api_utilizatori'),
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
 ]
