e93e4d49bfec367e18b6370b8871fde24de93539
mrfreemann1 <visovanionut6@gmail.com>
2026-06-04 20:20:59 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# Actualizăm pip și instalăm setuptools pentru a preveni eroarea pkg_resources
+pip install --upgrade pip setuptools wheel
+
+# Instalăm pachetele aplicației
 pip install -r requirements.txt
+
+# Rulăm comenzile Django
 python manage.py collectstatic --no-input
 python manage.py migrate
